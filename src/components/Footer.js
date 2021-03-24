f12d257eaab9c383633f5dd0e883a1a9cdc2caca
Ziyang-Wang <ziyang.wang777@gmail.com>
2021-03-24 16:39:22 -0500

    SatLink interactive

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+        <div className='footer'>
+          ©2020 StarLink Tracker. All Rights Reserved. Website Made by Ziyang Wang
+        </div>
+    );
+  }
+}
+
+export default Footer;
